15fac1b57723a5317906869a5b500c0cf7a51bab
Chris Kim <Chrisdjkim@gmail.com>
2020-06-23 04:08:55 -0400

    Commented out app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const request = require("request");
 
-const app = express();
+// const app = express();
 
-app.use(express.static('public'));
-
-
-app.listen(3000, function() {
-    console.log("Server is running on port 3000!")
-});
-
-app.get("/",function(req, res){
-    res.sendFile(__dirname + "/index.html") 
-})
+// app.use(express.static('public'));
 
 
+// app.listen(3000, function() {
+//     console.log("Server is running on port 3000!")
+// });
 
-// Why have a backend? I want to one day turn this timer into a 
-// multi-page app with the ability to log in, track time spent working (like a productivity app),etc
+// app.get("/",function(req, res){
+//     res.sendFile(__dirname + "/index.html") 
+// })
+
+
+
+// // Why have a backend? I want to one day turn this timer into a 
+// // multi-page app with the ability to log in, track time spent working (like a productivity app),etc
